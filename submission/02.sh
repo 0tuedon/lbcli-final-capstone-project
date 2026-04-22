f40360174eb00cf64611556de0f3f27bc7d8a0b2
0tuedon <tuedonmofe@gmail.com>
2026-04-22 23:49:03 +0100

    final project: This was great

--- submission/02.sh
+++ submission/02.sh
@@ -4,4 +4,4 @@ BLCK_NUMBER=243825
 
 BLCK_HASH=$(bitcoin-cli -signet getblockhash $BLCK_NUMBER)
 
-bitcoin-cli -signet getblock 0000000e120ff9801366e9c75f1fbe6b72cc6a577bf16a402fd1521c3653f24d 2 | jq '[.tx[].vout | length] | add'
+bitcoin-cli -signet getblock $BLCK_HASH 2 | jq '[.tx[].vout | length] | add'
